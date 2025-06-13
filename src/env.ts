35bce3c20dee07487b3559eb547b84668439c5f0
Rafaellucaas <rafaellucasjr123@gmail.com>
2025-06-13 14:12:46 -0300

    Amendments to the previous commit: Enhanced URL shortening with authentication
    - Remove JWT_SECRET from env schema
    - Create short URLs as anonymous users.
    - Use an Authorization token in the header to link the URL to their
    account.
    - Specify a customAlias for the URL when authenticated with a valid token.

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 const envSchema = z.object({
   MONGODB_USERNAME: z.string(),
   MONGODB_PASSWORD: z.string(),
-  JWT_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
